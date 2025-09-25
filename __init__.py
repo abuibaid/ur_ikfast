392652119e4395d0a1c93e4b0bab813f7d8b20b4
Khalil Abuibaid <khalilabuibaid@gmail.com>
2025-09-25 17:17:13 +0200

    Clear __init__.py of existing code
    Remove the content of __init__.py, including imports and comments.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-# ur_ikfast/__init__.py
 
-# Example: alias one solver as "ur_kinematics"
-from .ur5e_ikfast import *  # or ur10e_ikfast, depending on your robot
-
-# Provide a standard name
-import sys
-sys.modules[__name__ + ".ur_kinematics"] = sys.modules[__name__ + ".ur5e_ikfast"]
