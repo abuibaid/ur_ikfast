02310e46045bc6de9c344a705d084dfa5753662f
Khalil Abuibaid <khalilabuibaid@gmail.com>
2025-09-25 17:34:59 +0200

    Import Cython extensions and define __all__
    Added imports for various Cython extensions and defined __all__.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+# Import all the Cython extensions
+from . import ur5e_ikfast
+from . import ur3_ikfast
+from . import ur3e_ikfast
+from . import ur5_ikfast
+from . import ur10_ikfast
+from . import ur10e_ikfast
+from .ur_ikfast import ur_kinematics
+
+__all__ = ['ur5e_ikfast', 'ur3_ikfast', 'ur3e_ikfast', 'ur5_ikfast', 
+           'ur10_ikfast', 'ur10e_ikfast']
+
 
